cd76f03170499857ee7bd3d41dae15c574b9994d
MustafaShahoud <mustafa.shahoud@gmail.com>
2020-11-15 00:59:16 +0100

    Refactored

--- app/src/main/java/com/mustafa/newsapp/MainActivity.kt
+++ app/src/main/java/com/mustafa/newsapp/MainActivity.kt
@@ -2,15 +2,10 @@ package com.mustafa.newsapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.mustafa.newsapp.util.UiHelper
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    @Inject
-    lateinit var uiHelper: UiHelper
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/mustafa/newsapp/model/Article.kt
+++ app/src/main/java/com/mustafa/newsapp/model/Article.kt
@@ -1,14 +1,9 @@
 package com.mustafa.newsapp.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import java.io.Serializable
 
-@Entity(
-    tableName = "articles"
-)
+
 data class Article(
-    @PrimaryKey(autoGenerate = true)
     val id: Int? = null,
     val author: String?,
     val content: String?,
